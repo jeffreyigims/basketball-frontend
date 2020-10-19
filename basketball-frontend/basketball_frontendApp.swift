ad08a7f82d3964469942fc6f55dfea26d6b7c64c
Jeffrey Igims <jigims@andrew.cmu.edu>
2020-10-19 18:52:40 -0400

    initial

---
+++ basketball-frontend/basketball_frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  basketball_frontendApp.swift
+//  basketball-frontend
+//
+//  Created by Jeffrey Igims on 10/19/20.
+//
+
+import SwiftUI
+
+@main
+struct basketball_frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
